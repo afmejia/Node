b81b7695678eade33f48d55be8ed9bd5a5d0d3e7
Felipe <afmejia23@utp.edu.co>
2018-06-06 22:58:27 -0500

    debugging

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,18 +1,16 @@
-console.log('Starting notes.js');
-
 const fs = require('fs');
 
 var fetchNotes = () => {
-    try {
-        var notesString = fs.readFileSync('notes-data.json');
-        return JSON.parse(notesString);
-    } catch (e) {
-        return [];
-    }
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
 };
 
 var saveNotes = (notes) => {
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
 
 var addNote = (title, body) => {
@@ -24,36 +22,43 @@ var addNote = (title, body) => {
   var duplicateNotes = notes.filter((note) => note.title === title);
 
   if (duplicateNotes.length === 0) {
-      notes.push(note);
-      saveNotes(notes);
-      return note;
+    notes.push(note);
+    saveNotes(notes);
+    return note;
   }
 };
 
 var getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 
 var getNote = (title) => {
-  console.log('Reading note', title);
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
 };
 
 var removeNote = (title) => {
-    // fetch notes
-    var notes = fetchNotes();
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
 
-    // filter notes, removing  the one with title argument
-    var noteDeleted = notes.filter((note) => note.title !== title);
+  return notes.length !== filteredNotes.length;
+};
 
-    // save new notes array
-    saveNotes(noteDeleted);
-
-    return notes.length !== noteDeleted.length;
+var logNote = (note) => {
+    // Break on this line and use repl to output note
+    debugger;
+    // Use read command with --title
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
 };
 
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 };
