cd07d21b2b1a7da7bbdbbc52636913f7e131a115
Felipe Mejia <fmejia@sisfo.com>
2018-06-12 16:33:46 -0500

    Rendering injected data with hbs

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
-    res.send({
-        name: 'Felipe',
-        likes: [
-            'developing',
-            'training'
-        ]
+    // res.send({
+    //     name: 'Felipe',
+    //     likes: [
+    //         'developing',
+    //         'training'
+    //     ]
+    // });
+    res.render('home.hbs', {
+        Welcome: 'Welcome to my page',
+        currentYear: new Date().getFullYear(),
+        pageTitle: 'Home page'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'This is the About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req,res) => {
