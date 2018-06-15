eae7dde00ab7d0fcfbdbb5d71e8720268ce5cce8
Felipe Mejia <fmejia@sisfo.com>
2018-06-15 10:16:40 -0500

    adding git ignore

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+
+    if (req.url === '/maintenance') {
+        res.render('maintenance.hbs', );
+    }
+
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -41,7 +64,7 @@ app.get('/bad', (req,res) => {
         errorMessage: 'Unable to handle request'
     });
 });
-
+    
 app.listen(3000, () => {
     console.log('Serve is up on port 3000');
 });
